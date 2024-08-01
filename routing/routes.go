b34bbf117f93a3503a2673e751ca037488799ade
bhashimoto <brunothashimoto@gmail.com>
2024-08-01 11:33:25 -0300

    changed endpoints for api

--- routing/routes.go
+++ routing/routes.go
@@ -10,21 +10,21 @@ func SetRoutes(cfg *handlers.ApiConfig) (*http.ServeMux ){
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", cfg.HandleIndex)
 
-	mux.HandleFunc("GET /users", cfg.HandleGetUsers)
-	mux.HandleFunc("GET /users/{userID}", cfg.HandleGetUser)
-	mux.HandleFunc("POST /users", cfg.HandleCreateUser)
+	mux.HandleFunc("GET /api/users", cfg.HandleGetUsers)
+	mux.HandleFunc("GET /api/users/{userID}", cfg.HandleGetUser)
+	mux.HandleFunc("POST /api/users", cfg.HandleCreateUser)
 
-	mux.HandleFunc("POST /accounts", cfg.HandleAccountCreate)
-	mux.HandleFunc("GET /accounts", cfg.HandleAccountsGet)
-	mux.HandleFunc("GET /accounts/{accountID}", cfg.HandleAccountsGet)
-	mux.HandleFunc("GET /accounts/{accountID}/balance", cfg.HandleBalanceGet)
+	mux.HandleFunc("POST /api/accounts", cfg.HandleAccountCreate)
+	mux.HandleFunc("GET /api/accounts", cfg.HandleAccountsGet)
+	mux.HandleFunc("GET /api/accounts/{accountID}", cfg.HandleAccountsGet)
+	mux.HandleFunc("GET /api/accounts/{accountID}/balance", cfg.HandleBalanceGet)
 
-	mux.HandleFunc("POST /transactions", cfg.HandleTransactionCreate)
-	mux.HandleFunc("GET /transactions/{transactionID}", cfg.HandleTransactionGet)
+	mux.HandleFunc("POST /api/transactions", cfg.HandleTransactionCreate)
+	mux.HandleFunc("GET /api/transactions/{transactionID}", cfg.HandleTransactionGet)
 
-	mux.HandleFunc("POST /debts", cfg.HandleDebtCreate)
+	mux.HandleFunc("POST /api/debts", cfg.HandleDebtCreate)
 
-	mux.HandleFunc("POST /user-accounts", cfg.HandleUserAccountCreate)
+	mux.HandleFunc("POST /api/user-accounts", cfg.HandleUserAccountCreate)
 
 	return mux
 }
